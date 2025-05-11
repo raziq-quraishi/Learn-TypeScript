563c0d26d2ca0d0433ec30c164aa00106486a745
Abdul Raziq Quraishi <abdulraziqquraishi97@gmail.com>
2025-05-11 05:27:00 -0700

    Learning TypeScript for the first time

---
+++ app.js
@@ -0,0 +1,16 @@
+// // Explicit Type
+// let firstName:string="Ab Raziq"
+// // Implicit Type
+// let lastName= "Quraishi"
+// // firstName =28;
+// // lastName=243
+// console.log(firstName + lastName)
+// const json= JSON.parse("55");
+// console.log(typeof)
+// -----------------TypeScript Arrays
+// const names:  string[]=[];
+//  Readonly Type
+var names = ["Hamid"];
+names.push("Ab Raziq");
+names.push(28);
+console.log(names);
---
+++ app.ts
@@ -0,0 +1,29 @@
+
+
+// // Explicit Type
+// let firstName:string="Ab Raziq"
+
+// // Implicit Type
+// let lastName= "Quraishi"
+
+
+// // firstName =28;
+// // lastName=243
+// console.log(firstName + lastName)
+
+// const json= JSON.parse("55");
+
+// console.log(typeof)
+
+
+
+// -----------------TypeScript Arrays
+
+// const names:  string[]=[];
+
+//  Readonly Type
+const names : readonly string[]=["Hamid"]
+
+names.push("Ab Raziq")
+names.push(28)
+console.log(names)
